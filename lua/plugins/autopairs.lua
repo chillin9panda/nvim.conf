929793428263e6725b402c51be907e01f2a7a680
chillin9panda <duremarkastu@gmail.com>
2025-05-18 09:48:43 +0300

    typo fix

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -14,7 +14,7 @@ return {
 		"windwp/nvim-ts-autotag",
 		config = function()
 			require("nvim-ts-autotag").setup({
-				opts = {
+				options = {
 					enable_close = true,
 					enable_rename = true,
 					enable_close_on_slash = false,
