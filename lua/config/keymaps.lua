df923bb97542234baae3b4e34a24d16ec4ccbc0a
chillin9panda <duressamarkos9@gmail.com>
2025-05-27 07:14:54 +0300

    search cancel fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,9 @@
 -- Disable arrow keys in normal, insert, and visual modes
 for _, mode in ipairs({ "n", "i", "v" }) do
-  vim.keymap.set(mode, "<Up>", "<Nop>", { desc = "Disable Up Arrow" })
-  vim.keymap.set(mode, "<Down>", "<Nop>", { desc = "Disable Down Arrow" })
-  vim.keymap.set(mode, "<Left>", "<Nop>", { desc = "Disable Left Arrow" })
-  vim.keymap.set(mode, "<Right>", "<Nop>", { desc = "Disable Right Arrow" })
+	vim.keymap.set(mode, "<Up>", "<Nop>", { desc = "Disable Up Arrow" })
+	vim.keymap.set(mode, "<Down>", "<Nop>", { desc = "Disable Down Arrow" })
+	vim.keymap.set(mode, "<Left>", "<Nop>", { desc = "Disable Left Arrow" })
+	vim.keymap.set(mode, "<Right>", "<Nop>", { desc = "Disable Right Arrow" })
 end
 
-vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
