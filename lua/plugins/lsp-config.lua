b39babf5bc248ac8b61536432a9d46e71ab1422e
chillin9panda <duressamarkos9@gmail.com>
2025-07-10 13:04:38 +0300

    bunch of stuff mainly for java

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,56 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+          vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+        end,
+      })
 
-			local on_attach = function(_, bufnr)
-				local opts = { buffer = bufnr }
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, {}, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}, opts)
-				vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {}, opts)
-			end
+      local lspconfig = require("lspconfig")
+      local servers = {
+        "lua_ls",
+        -- "jdtls",
+        "clangd",
+        "ts_ls",
+        "intelephense",
+        "bashls",
+        "html",
+        "csharp_ls",
 
-			local lspconfig = require("lspconfig")
-			local servers = {
-				"lua_ls",
-				"jdtls",
-				"clangd",
-				"ts_ls",
-				"intelephense",
-				"bashls",
-				"html",
-				"csharp_ls",
+        -- Add as needed
+      }
 
-				-- Add as needed
-			}
-
-			for _, server in ipairs(servers) do
-				lspconfig[server].setup({
-					capabilities = capabilities,
-					on_attach = on_attach,
-				})
-			end
-		end,
-	},
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+        })
+      end
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.shfmt,
         --require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.prettier,
@@ -18,8 +18,8 @@ return {
         null_ls.builtins.formatting.stylelint,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.google_java_format,
+        -- null_ls.builtins.formatting.clang_format,
         --null_ls.builtins.formatting.csharpier,
       },
 
