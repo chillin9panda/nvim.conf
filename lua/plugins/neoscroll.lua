8cc5400a8c1f73fb8efc745077f3dc7daf08a0f6
chillin9panda <duressamarkos9@gmail.com>
2025-11-06 18:45:49 +0300

    added scroll animation plugin

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,11 @@
+return {
+  "karb94/neoscroll.nvim",
+  opts = {
+    mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb' },
+    hide_cursor = true,
+    stop_eof = true,
+    respect_scrolloff = true,
+    easing_function = "quadratic",
+    duration = 100,
+  },
+}
