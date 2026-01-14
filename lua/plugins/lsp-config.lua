05c7b8b71c5f42c149ee7a0507c00edf4d59671d
chillin9panda <duressamarkos9@gmail.com>
2026-01-14 19:33:19 +0300

    lsp error shows all files from buffer instead of only open file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,7 +28,11 @@ return {
           vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
           vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-          vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
+          vim.keymap.set("n", "<leader>q", function()
+              vim.diagnostic.setqflist({ open = true })
+            end,
+            opts
+          )
         end,
       })
 
