1db5c9b9ff95823f531618a11578995614d6ba74
chillin9panda <duressamarkos9@gmail.com>
2026-01-24 21:32:18 +0300

    fix indent

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,30 +1,30 @@
 -- switch to conform.nvim
 
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        -- null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylelint,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.phpcsfixer,
-      },
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.formatting.stylelint,
+				-- null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.phpcsfixer,
+			},
 
-      -- Auto format on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      }),
+			-- Auto format on save
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			}),
 
-      vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {}),
-    })
-  end,
+			vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {}),
+		})
+	end,
 }
