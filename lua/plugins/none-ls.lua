5fba772a719ad69510642e44f755e32168dec9d7
Duressa M <duressamarkos9@gmail.com>
2026-03-15 21:47:36 +0300

    Commented out formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,10 +10,10 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.formatting.prettier,
 				-- null_ls.builtins.formatting.stylelint,
 				-- null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.phpcsfixer,
+				-- null_ls.builtins.formatting.phpcsfixer,
 			},
 
 			-- Auto format on save
