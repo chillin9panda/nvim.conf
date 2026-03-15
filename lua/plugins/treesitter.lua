71c65eb311fbaf39d974a1e81a9716845af4b4ff
Duressa M <duressamarkos9@gmail.com>
2026-03-15 22:47:13 +0300

    rollback treesitter breaking update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+  commit = "42fc28ba918343ebfd5565147a42a26580579482",
 	build = ":TSUpdate",
 
 	config = function()
-		local config = require("nvim-treesitter")
+		local config = require("nvim-treesitter.configs")
 		config.setup({
 			auto_install = true,
 			highlight = { enable = true },
