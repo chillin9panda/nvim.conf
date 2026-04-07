3658ce69c0587006818d82ffe2e62c3914b086b5
chillin9panda <duressamarkos9@gmail.com>
2026-04-07 16:09:19 +0300

    treesitter reconfig

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,51 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-  commit = "42fc28ba918343ebfd5565147a42a26580579482",
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    local ts = require("nvim-treesitter")
 
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    ts.setup({
+      install_dir = vim.fn.stdpath('data') .. '/site'
+    })
+
+    ts.install({
+      "python", "bash", "cpp", "c_sharp", "css", "javascript", "typescript",
+      "php", "java", "kotlin", "lua", "cmake", "scss",
+      "xml", "html", "yaml", "json", "markdown", "markdown_inline",
+      -- Neovim internal essentials
+      "vim", "vimdoc", "query", "regex", "lua"
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local bufnr = args.buf
+
+        if vim.bo[bufnr].buftype ~= "" then
+          return
+        end
+
+        local ft = vim.bo[bufnr].filetype
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+
+        local ok, _ = pcall(vim.treesitter.get_parser, bufnr, lang)
+        if ok then
+          vim.treesitter.start(bufnr)
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(args)
+        local bufnr = args.buf
+        if vim.bo[bufnr].buftype == "" then
+          local ft = vim.bo[bufnr].filetype
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+          if pcall(vim.treesitter.get_parser, bufnr, lang) then
+            vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end
+      end,
+    })
+  end,
 }
