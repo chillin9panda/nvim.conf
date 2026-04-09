8c55d906117d973e88099a9f8a708f5930d8dd93
chillin9panda <duressamarkos9@gmail.com>
2026-04-09 12:45:31 +0300

    Remove depricated call and key remapping

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -34,7 +34,7 @@ return {
           vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
           vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.jump, opts)
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
           vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
           vim.keymap.set("n", "<leader>q", function()
