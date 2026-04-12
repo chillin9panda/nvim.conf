3fd151ab33613dcd1813eaddaeadbe7946ca5b38
chillin9panda <duressamarkos9@gmail.com>
2026-04-12 11:30:10 +0300

    fix ts not loading for somefiles

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,31 +11,25 @@ return {
 
     ts.install({
       "python", "bash", "cpp", "c_sharp", "css", "javascript", "typescript",
-      "tsx", "jsx", "ini", "ssh_config", "git_config",
+      "jsx", "tsx", "ini", "ssh_config", "git_config",
       "php", "java", "kotlin", "lua", "cmake", "scss",
       "xml", "html", "yaml", "json", "markdown", "markdown_inline",
-      -- Neovim internal essentials
-      "vim", "vimdoc", "query", "regex", "lua"
+      "vim", "vimdoc", "query", "regex"
     })
 
     vim.api.nvim_create_autocmd('FileType', {
       callback = function(args)
         local bufnr = args.buf
 
-        if vim.bo[bufnr].buftype ~= "" then
-          return
-        end
+        if vim.bo[bufnr].buftype ~= "" then return end
 
         local ft = vim.bo[bufnr].filetype
         local lang = vim.treesitter.language.get_lang(ft) or ft
 
-        local has_lang = pcall(vim.treesitter.language.add, lang)
+        if pcall(vim.treesitter.language.add, lang) then
+          vim.treesitter.start(bufnr, lang)
 
-        if has_lang then
-          local ok, _ = pcall(vim.treesitter.get_parser, bufnr, lang)
-          if ok then
-            vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-          end
+          vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end
       end,
     })
